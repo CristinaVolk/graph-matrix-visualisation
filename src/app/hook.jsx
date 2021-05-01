6f4593298e982408f31b3c6414aba4e37acaca18
VolkCristina <kvolk@dynak.com>
2021-05-01 18:18:02 +0100

    functionality of pop up information box [extracted]

--- src/app/hook.jsx
+++ src/app/hook.jsx
@@ -4,6 +4,8 @@ import { useGetCharMatrixData } from "./useGetCharMatrixData";
 
 export function useComponent() {
   const [chart, setChart] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
 
   const {
     chartContent,
@@ -13,21 +15,44 @@ export function useComponent() {
     setChartContent,
   } = useGetCharMatrixData();
 
+  const loadedChart = useCallback((chart) => {
+    setChart(chart);
+  }, []);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const clickNodeHandler = useCallback(
+    ({ id }) => {
+      if (chart.getItem(id)) {
+        setSelectedItem(chart.getItem(id));
+        handleClickOpen();
+      }
+    },
+    [chart],
+  );
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedItem(value);
+  };
+
   useEffect(() => {
     fetchCharMatrixData();
 
     return () => clearError();
   }, [clearError, fetchCharMatrixData]);
 
-  const loadedChart = useCallback((chart) => {
-    setChart(chart);
-  }, []);
-
   return {
     chartContent,
     loading,
     chart,
+    open,
+    selectedItem,
     loadedChart,
     setChartContent,
+    clickNodeHandler,
+    handleClose,
   };
 }
