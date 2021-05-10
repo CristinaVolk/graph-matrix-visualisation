5383cfdc8335424aced663727c6676a67557bb66
VolkCristina <kvolk@dynak.com>
2021-05-10 15:07:55 +0100

    test for the Loading component [updated]

--- src/components/Loading.test.js
+++ src/components/Loading.test.js
@@ -4,12 +4,20 @@ import { render } from "@testing-library/react";
 import { Loading } from "./Loading";
 
 describe("Loading component", () => {
-  it("renders the Linear Loading Mui component", () => {
-    const { queryAllByRole } = render(<Loading />);
-    const progressBarList = queryAllByRole("progressbar");
+  it("renders 2 Linear Loading Mui components", () => {
+    const { getAllByRole } = render(<Loading />);
+    const progressBarList = getAllByRole("progressbar");
+    const classNamesList = [
+      "MuiLinearProgress-colorPrimary",
+      "MuiLinearProgress-colorSecondary",
+    ];
+
     expect(progressBarList.length).toEqual(2);
-    progressBarList.forEach((singleItem) =>
-      expect(singleItem).toBeInstanceOf(HTMLDivElement),
-    );
+    progressBarList.forEach((singleItem, index) => {
+      expect(singleItem).toBeInstanceOf(HTMLDivElement);
+      expect(
+        singleItem.className.split(" ").includes(classNamesList[index]),
+      ).toBeTruthy();
+    });
   });
 });
